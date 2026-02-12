9ebcb7f8a0693e142895cd001bc30b04aff331ed
abhinaysharma-devil <panchalabhinay@gmail.com>
2026-02-12 19:31:36 +0530

    port changes

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   const port = configService.get("PORT") || 3000;
 
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 
   console.log(`Server running on port ${port}`);
 }
