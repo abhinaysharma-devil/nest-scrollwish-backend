c5380f5062932f6fbdaad37e41d060b03373c4b0
abhinaysharma-devil <panchalabhinay@gmail.com>
2026-02-13 11:39:19 +0530

    added cosnsole for PORT

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
 
+  console.log(">>>>>>>>>>>>>>>>>>>>>> PORT ", configService.get("PORT"));
+
   const port = configService.get("PORT") || 3000;
 
   await app.listen(port, "0.0.0.0");
